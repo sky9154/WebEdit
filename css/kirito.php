ccd133999d7cafc0f576fc6d37ca984757a2645c
oF <60073419+sky0966548546@users.noreply.github.com>
2021-07-17 04:03:59 +0800

    Add files via upload

---
+++ css/kirito.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Logo -->
+    <link rel="icon" type="image/x-icon" href="https://kiriqua.com/images/logo.png" />
+    <link rel="shortcut icon" href="https://kiriqua.com/images/logo.png" type="image/x-icon" />
+    <title>畫面預覽</title>
+</head>
+<body>
+<div>
+    <?php
+        $css="";$css=$_POST["css"];
+        echo "<div class='center-align' style=\"".$css."\">TEXTtext<br>測試測試</div>"
+    ?>
+</div>
+</body>
+</html>
